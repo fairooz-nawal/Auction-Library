2dd1b22a7952b7a2be21efcacf932b1bbdf339f0
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-04-09 23:40:42 +0600

    Adding the Toastify Message

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,40 @@ import Footer from './components/footer/Footer'
 import Bidding from './components/bidding/Bidding'
 import Favourite from './components/favourite/Favourite'
 import { Suspense, useState } from 'react'
+import { ToastContainer, toast } from "react-toastify";
+
 function App() {
   const [markedBook, setMarkedBook] = useState([]);
   const bidding = fetch("./bidding.json").then(res => res.json());
   const handleAddBookMark = (id) => {
-    setMarkedBook([...markedBook,id]);
+    setMarkedBook([...markedBook, id]);
+    toast('Item is added to Favourite Section Successfully!!', {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
   }
   console.log(markedBook);
   return (
     <>
       <div className="sora-regular max-w-full bg-gray-200">
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         <Navbar></Navbar>
         <Banner></Banner>
         <div className="w-full p-[100px] space-y-4">
@@ -24,7 +48,7 @@ function App() {
           <div className="flex  mx-auto gap-6">
             <Suspense fallback={<div><span className="loading loading-bars loading-xl"></span></div>}>
               <Bidding bidding={bidding}
-                       handleAddBookMark={handleAddBookMark}>
+                handleAddBookMark={handleAddBookMark}>
               </Bidding>
             </Suspense>
             <Favourite markedBook={markedBook}>
