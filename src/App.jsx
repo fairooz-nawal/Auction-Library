335be19bf30765147da577975db2ec9deb5906fa
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-04-09 18:31:38 +0600

    Initial Setup of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+function App() {
+  
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
